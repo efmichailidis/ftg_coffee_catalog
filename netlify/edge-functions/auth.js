53e4825a946c841b6b571222b4dd6ade59bc8767
efmichailidis <emichailidis@sklavenitis.co.gr>
2026-07-27 11:03:03 +0300

    Create auth.js

---
+++ netlify/edge-functions/auth.js
@@ -0,0 +1,24 @@
+export default async (request, context) => {
+  // Ορίστε το username και το password που θέλετε
+  const USERNAME = "myuser";
+  const PASSWORD = "mypassword123";
+
+  const authHeader = request.headers.get("authorization");
+
+  if (authHeader) {
+    const auth = atob(authHeader.split(" ")[1]).split(":");
+    const user = auth[0];
+    const pass = auth[1];
+
+    if (user === USERNAME && pass === PASSWORD) {
+      return await context.next();
+    }
+  }
+
+  return new Response("Unauthorized", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Restricted Area"',
+    },
+  });
+};
